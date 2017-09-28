1202c4c866bc50b38f1c6b9f425555dc2b8c9611
huyhoangt2 <01659039498>
2017-09-28 09:30:55 +0700

    commit local contract

--- app/controllers/ProfileContractController.scala
+++ app/controllers/ProfileContractController.scala
@@ -1,45 +1,48 @@
 package controllers
 
+import com.ftel.bigdata.utils.DomainUtil
+
 import javax.inject.Inject
 import javax.inject.Singleton
 import play.api.data.Form
 import play.api.data.Forms.mapping
 import play.api.data.Forms.text
-import play.api.mvc.AbstractController
-import play.api.mvc.ControllerComponents
-import play.api.mvc.AnyContent
-import play.api.mvc.Request
-import services.CacheService
+import play.api.db.slick.DatabaseConfigProvider
+import play.api.db.slick.HasDatabaseConfigProvider
 import play.api.i18n.I18nSupport
-import services.domain.ClientService
-import services.domain.CommonService
+import play.api.mvc.AbstractController
+import play.api.mvc.AnyContent
+import play.api.mvc.ControllerComponents
+import play.api.mvc.Request
+import services.paytv.ProfileService
+import slick.jdbc.JdbcProfile
+
+case class SearchContract(q: String)
 
 /**
   * This controller creates an `Action` to handle HTTP requests to the
   * application's home page.
   */
 @Singleton
-class ProfileContractController @Inject() (cc: ControllerComponents) extends AbstractController(cc) with I18nSupport {
-
+class ProfileContractController @Inject() (protected val dbConfigProvider: DatabaseConfigProvider, cc: ControllerComponents)
+  extends AbstractController(cc) with HasDatabaseConfigProvider[JdbcProfile] with I18nSupport {
 
   val form = Form(
     mapping(
-      "q" -> text
-    )(SearchData.apply)(SearchData.unapply)
+      "ct" -> text
+    )(SearchContract.apply)(SearchContract.unapply)
   )
 
   def index = Action { implicit request: Request[AnyContent] =>
-    /*val formValidationResult = form.bindFromRequest
+    val formValidationResult = form.bindFromRequest
     if (!formValidationResult.hasErrors) {
-      val ip = formValidationResult.get.q
-      val response = CacheService.getClient(ip)
-      Ok(views.html.dns.profile.client.index(form, response._1, null, ip))
+      val domain = formValidationResult.get.q
+      val response = ProfileService.get(domain)
+      Ok(views.html.dns_v2.profile.contract.index(form, response))
     } else {
-      Ok(views.html.dns.profile.client.index(form, null, ClientService.getTop(), ""))
-    }*/
-    Ok(views.html.dns_v2.profile.contract.index())
+      Ok(views.html.dns_v2.profile.contract.index(form, null))
+    }
   }
-
 }
 
 
