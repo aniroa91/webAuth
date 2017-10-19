5235397bb6530cae3e4a30506f1d3cbd19793478
huyhoangt2 <01659039498>
2017-10-19 12:46:18 +0700

    add device page and fix menu left

---
+++ app/controllers/DeviceController.scala
@@ -0,0 +1,21 @@
+package controllers
+
+import javax.inject.Inject
+import javax.inject.Singleton
+import play.api.mvc.AbstractController
+import play.api.mvc.ControllerComponents
+import services.CacheService
+
+/**
+  * This controller creates an `Action` to handle HTTP requests to the
+  * application's home page.
+  */
+@Singleton
+class DeviceController @Inject()(cc: ControllerComponents) extends AbstractController(cc) with Secured{
+
+  def index =  withAuth { username => implicit request =>
+    Ok(views.html.device.index(username))
+  }
+}
+
+
