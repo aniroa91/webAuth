30e6175c000f79e6d5354a695f40d657f2d511ff
Dung Vo <cdung.vo@gmail.com>
2017-10-23 15:33:35 +0700

    Update build

--- build.sh
+++ build.sh
@@ -2,13 +2,7 @@
 
 NAME=bigdata-play
 
-#docker build --no-cache -t $NAME .
+docker build --no-cache -t $NAME .
 docker stop $NAME
 docker rm $NAME
-docker run -d \
-           --net=host \
-           --name $NAME \
-           -p 80:9000 \
-           -v /public/images/:/opt/bigdata-play/public/ \
-           -v ${PWD}/dist:/opt/bigdata-play \
-           bigdata-registry.local:5043/java /opt/bigdata-play/bin/bigdata-play -Dconfig.file=conf/prod/application.conf
+docker run -d --net=host --name $NAME -p 80:9000 -v /public/images/:/opt/bigdata-play/public/ $NAME
