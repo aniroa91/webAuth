00ef3e868419a94609ed4a5e3a9ca5b22c86f11a
Dung Vo <cdung.vo@gmail.com>
2017-10-23 15:19:01 +0700

    Change build.sh

--- build.sh
+++ build.sh
@@ -2,7 +2,13 @@
 
 NAME=bigdata-play
 
-docker build --no-cache -t $NAME .
+#docker build --no-cache -t $NAME .
 docker stop $NAME
 docker rm $NAME
-docker run --net=host --name $NAME -p 80:9000 -v /public/images/:/opt/bigdata-play/public/ -d $NAME
+docker run -d \
+           --net=host \
+           --name $NAME \
+           -p 80:9000 \
+           -v /public/images/:/opt/bigdata-play/public/ \
+           -v ${PWD}/dist:/opt/bigdata-play \
+           bigdata-registry.local:5043/java /opt/bigdata-play/bin/bigdata-play -Dconfig.file=conf/prod/application.conf
