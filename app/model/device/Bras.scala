e2ce3fb03bacb959d4137d90d4224ecc46b6abea
huyhoangt2 <01659039498>
2017-11-09 10:08:39 +0700

    update device

--- app/model/device/Bras.scala
+++ app/model/device/Bras.scala
@@ -13,9 +13,9 @@ import slick.driver.PostgresDriver.api._
 
 import scala.concurrent.ExecutionContext.Implicits.global
 
-case class Bras(id: String, time: String,logoff: Int, signin: Int,lostip_error: Int, active_user: Int,crit_kibana: Int, crit_opsview: Int,cpe_error: Int, label: String)
+case class Bras(id: String, time: String,logoff: Int, signin: Int,lostip_error: Int, active_user: Int,crit_kibana: Int, crit_opsview: Int,cpe_error: Int, label: String,verified: String)
 
-class BrasTableDef(tag: Tag) extends Table[Bras](tag, "dwh_radius_bras_detail2") {
+class BrasTableDef(tag: Tag) extends Table[Bras](tag, "dwh_radius_bras_detail") {
 
   def id = column[String]("bras_id")
   def time = column[String]("date_time")
@@ -27,9 +27,10 @@ class BrasTableDef(tag: Tag) extends Table[Bras](tag, "dwh_radius_bras_detail2")
   def crit_opsview = column[Int]("crit_opsview")
   def cpe_error = column[Int]("cpe_error")
   def label = column[String]("label")
+  def verified = column[String]("verified")
 
   override def * =
-    (id, time,logoff, signin,lostip_error, active_user,crit_kibana, crit_opsview,cpe_error, label) <>(Bras.tupled, Bras.unapply)
+    (id, time,logoff, signin,lostip_error, active_user,crit_kibana, crit_opsview,cpe_error, label,verified) <>(Bras.tupled, Bras.unapply)
 }
 
 object BrasList {
@@ -38,11 +39,21 @@ object BrasList {
 
   var brases = TableQuery[BrasTableDef]
 
-  def top100: Future[Seq[Bras]] = {
+  def top100: Future[Seq[(String,String,String,String,String, String, String, String,String,String)]] = {
     val dt = new DateTime();
-    val oneHoursLater = dt.minusHours(24);
-    val oldHour  = oneHoursLater.toString(DateTimeFormat.forPattern("yyyy-MM-dd HH:mm:ss.SSS"))
-    dbConfig.db.run(brases.sortBy(_.time.desc).filter(_.time >= oldHour).filter(_.label ==="outlier").take(100).result)
+    val oneDayLater = dt.minusHours(48);
+    val oldDay  = oneDayLater.toString(DateTimeFormat.forPattern("yyyy-MM-dd HH:mm:ss.SSS"))
+   // dbConfig.db.run(brases.sortBy(_.time.desc).filter(_.time >= oldDay).filter(_.label ==="outlier").take(100).result)
+    dbConfig.db.run(
+      sql"""select tbD.bras_id,tbD.date_time,tbD.active_user,tbD.signin_total_count,tbD.logoff_total_count,tbD.lostip_error,tbD.cpe_error,tbD.crit_kibana,tbD.crit_opsview,tbD.verified
+            from
+            (select bras_id,max(date_time) as time from dwh_radius_bras_detail
+            where label='outlier'
+            group by bras_id) tbB
+            join dwh_radius_bras_detail tbD
+            on tbD.bras_id = tbB.bras_id and tbD.date_time=tbB.time and tbD.date_time>=$oldDay::TIMESTAMP
+                  """
+        .as[(String, String, String, String,String,String, String, String, String,String)])
   }
 
 /*  def get(id: String,time: String): Future[Option[Bras]] = {
@@ -54,7 +65,7 @@ object BrasList {
     val dateTime = DateTime.parse(time, formatter)
     val oldTime  = dateTime.minusHours(1).toString(DateTimeFormat.forPattern("yyyy-MM-dd HH:mm:ss.SSS"))
 
-    dbConfig.db.run(brases.filter(_.id === id).filter(_.time >= oldTime).filter(_.time <= time).sortBy(_.time.desc).take(5).result)
+    dbConfig.db.run(brases.filter(_.id === id).filter(_.time >= oldTime).filter(_.time <= time).filter(_.label ==="outlier").sortBy(_.time.desc).take(5).result)
   }
 
   def getChart(id: String,time: String): Future[Seq[Bras]] = {
@@ -64,6 +75,22 @@ object BrasList {
     val addHalfHour  = dateTime.plusMinutes(30).toString(DateTimeFormat.forPattern("yyyy-MM-dd HH:mm:ss"))
 
     dbConfig.db.run(brases.filter(_.id === id).filter(_.time >= oldHalfHour).filter(_.time <= addHalfHour).sortBy(_.time.asc).result)
+  }
+
+  def confirmLabel(id: String,time: String) = {
+    dbConfig.db.run(
+      sqlu"""UPDATE dwh_radius_bras_detail SET verified =1
+            where bras_id =$id and date_time>=$time::TIMESTAMP
+                  """
+    )
+  }
+
+  def rejectLabel(id: String,time: String) = {
+    dbConfig.db.run(
+      sqlu"""UPDATE dwh_radius_bras_detail SET verified =0
+            where bras_id =$id and date_time>=$time::TIMESTAMP
+                  """
+    )
   }
 
 }
--- app/model/device/BrasCard.scala
+++ app/model/device/BrasCard.scala
@@ -31,19 +31,63 @@ object BrasesCard {
 
   var brases = TableQuery[BrasCardTableDef]
 
-  def listBrasOutlier: Future[Seq[(String,String,String,String)]] = {
+  def getHostCard(strId: String): Future[Seq[(String,String,String, String)]] = {
+    val id = strId.split('/')(0)
+    val time = strId.split('/')(1)
+    val formatter = DateTimeFormat.forPattern("yyyy-MM-dd HH:mm:ss.SSS")
+    val dateTime = DateTime.parse(time, formatter)
+    val oldTime  = dateTime.minusHours(1).toString(DateTimeFormat.forPattern("yyyy-MM-dd HH:mm:ss.SSS"))
+    dbConfig.db.run(
+      sql"""SELECT host,module,sum(cpe_error),sum(lostip_error) from dwh_inf_module
+            WHERE bras_id=$id and date_time<=$time::TIMESTAMP and date_time>=$oldTime::TIMESTAMP
+            GROUP BY host,module
+                  """
+        .as[(String, String,String, String)])
+  }
+
+  def listNocOutlier: Future[Seq[(String,String)]] = {
+    val dt = new DateTime();
+    val aHourLater = dt.minusHours(24);
+    val aHourTime = aHourLater.toString(DateTimeFormat.forPattern("yyyy-MM-dd HH:mm:ss.SSS"))
+    dbConfig.db.run(
+      sql"""SELECT tb.province,count(distinct tb.numBras)
+            FROM(
+              select substring(bras_id,1, 3) as province,substring(bras_id,5, length(bras_id)) as numBras  from dwh_radius_bras_detail
+              where date_time>=$aHourTime::TIMESTAMP and label ='outlier'
+              order by date_time desc
+              ) tb
+            GROUP BY tb.province
+                  """
+        .as[(String,String)])
+  }
+
+  def listBrasById(id: String): Future[Seq[(String,String,String,String,String)]] = {
+    val dt = new DateTime();
+    val aDayLater = dt.minusMinutes(60);
+    val aDayTime = aDayLater.toString(DateTimeFormat.forPattern("yyyy-MM-dd HH:mm:ss.SSS"))
+    dbConfig.db.run(
+      sql"""SELECT distinct tbC.bras_id,tbB.date_time,tbC.line_ol,tbC.card_ol,tbC.port_ol
+             FROM (SELECT bras_id,date_time FROM dwh_radius_bras_detail WHERE date_time >= $aDayTime::TIMESTAMP and label = 'outlier' and bras_id  LIKE $id || '%') tbB join
+               (SELECT * FROM bras_count_by_port WHERE bras_id LIKE  $id || '%') tbC
+             on tbB.bras_id=tbC.bras_id and  date_trunc('minute', tbC.time) between date_trunc('minute', tbB.date_time) - INTERVAL '3' MINUTE and date_trunc('minute', tbB.date_time)
+             ORDER BY tbC.bras_id desc,tbC.line_ol desc,tbC.port_ol desc,tbB.date_time desc
+                  """
+        .as[(String, String, String, String,String)])
+  }
+
+  def listBrasOutlier: Future[Seq[(String,String,String,String,String)]] = {
    // try {
       val dt = new DateTime();
-      val threeHoursLater = dt.minusHours(6);
-      val threeTime = threeHoursLater.toString(DateTimeFormat.forPattern("yyyy-MM-dd HH:mm:ss.SSS"))
+      val aDayLater = dt.minusHours(1);
+      val aDayTime = aDayLater.toString(DateTimeFormat.forPattern("yyyy-MM-dd HH:mm:ss.SSS"))
       dbConfig.db.run(
-        sql"""SELECT tbC.bras_id,tbC.time,tbC.line_ol,tbC.card_ol
-                 FROM (SELECT bras_id,date_time FROM dwh_radius_bras_detail2 WHERE label = 'outlier' and date_time >= $threeTime::TIMESTAMP) tbB join
-                 (SELECT * FROM bras_count_by_card WHERE time >= $threeTime::TIMESTAMP) tbC
+        sql"""SELECT tbC.bras_id,tbC.time,tbC.line_ol,tbC.card_ol,tbC.port_ol
+                 FROM (SELECT bras_id,date_time FROM dwh_radius_bras_detail WHERE label = 'outlier' and date_time >= $aDayTime::TIMESTAMP) tbB join
+                 (SELECT * FROM bras_count_by_port WHERE time >= $aDayTime::TIMESTAMP) tbC
                   on tbB.bras_id=tbC.bras_id and to_char(tbB.date_time, 'YYYY-MM-DD HH:mm:ss') = to_char(tbC.time, 'YYYY-MM-DD HH:mm:ss')
-                  ORDER BY tbC.bras_id desc,tbC.line_ol desc,tbC.time desc limit 10
+                  ORDER BY tbC.bras_id desc,tbC.line_ol desc,tbC.port_ol desc,tbC.time desc limit 10
                   """
-          .as[(String, String, String, String)])
+          .as[(String, String, String, String,String)])
     //}finally dbConfig.db.close
   }
 
@@ -64,10 +108,15 @@ object BrasesCard {
         .as[(String, String,String,String)])
   }
 
-  def getCard(id: String,time: String,sigin: String,logoff: String): Future[Seq[(String,String,String,String,Int,Int)]] = {
+  def getCard(id: String,time: String,sigin: String,logoff: String): Future[Seq[(String,String,String,Int,Int)]] = {
+    val formatter = DateTimeFormat.forPattern("yyyy-MM-dd HH:mm:ss.SSS")
+    val dateTime = DateTime.parse(time, formatter)
+    val oldTime  = dateTime.minusMinutes(5).toString(DateTimeFormat.forPattern("yyyy-MM-dd HH:mm:ss.SSS"))
+    val newTime  = dateTime.plusMinutes(5).toString(DateTimeFormat.forPattern("yyyy-MM-dd HH:mm:ss.SSS"))
       dbConfig.db.run(
-        sql"""SELECT bras_id,time,line_ol, card_ol,SUM(logoff_total_count_by_card),SUM(signin_total_count_by_card)
-              FROM bras_count_by_card WHERE bras_id=$id AND time = $time::TIMESTAMP GROUP BY bras_id,time,line_ol, card_ol ORDER BY line_ol, card_ol"""
-        .as[(String,String,String,String,Int,Int)])
+        sql"""SELECT bras_id,line_ol, card_ol,SUM(logoff_total_count_by_card),SUM(signin_total_count_by_card)
+              FROM bras_count_by_card WHERE bras_id=$id AND time>=$oldTime::TIMESTAMP AND time <= $newTime::TIMESTAMP GROUP BY bras_id,line_ol, card_ol ORDER BY line_ol, card_ol"""
+        .as[(String,String,String,Int,Int)])
   }
+
 }
