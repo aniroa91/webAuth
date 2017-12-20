88a56fd5db1b73bddfdaf21f19831e658afcc5d3
Dung Chi Vo <cdung.vo@gmail.com>
2017-12-20 16:56:50 +0700

    Update chart daily query/client for compare page

--- app/controllers/CompareController.scala
+++ app/controllers/CompareController.scala
@@ -8,6 +8,7 @@ import services.CacheService
 import java.text.NumberFormat
 import com.ftel.bigdata.utils.NumberUtil
 import dns.utils.DataSampleUtil
+import com.ftel.bigdata.utils.DateTimeUtil
 
 /**
   * This controller creates an `Action` to handle HTTP requests to the
@@ -19,13 +20,31 @@ class CompareController @Inject()(cc: ControllerComponents) extends AbstractCont
   def index(domains: String) =  Action {
 //    Ok(views.html.compare.index())
 //    Ok(domains)
-    val responses = domains.split(",").map(x => x -> CacheService.getDomain(x)._1).toMap
+    val responses = domains.split(",").map(x => x -> CacheService.getDomain(x)._1).toMap.filter(x => x._2 != null)
+    val now = DateTimeUtil.now
     
+    val dailyCate = (1 until 30).toArray.map(x => now.minusDays(x).toString("yyyy-MM-dd"))
+    
+    val dailyQueries = responses.map(x => {
+      val second = x._1
+      val history = x._2.history.map(y => y.day -> y.queries).toMap
+      second -> dailyCate.map(y => history.getOrElse(y, 0L))
+      }).toArray
+      
+    val dailyClients = responses.map(x => {
+      val second = x._1
+      val history = x._2.history.map(y => y.day -> y.clients).toMap
+      second -> dailyCate.map(y => history.getOrElse(y, 0L))
+      }).toArray
+      
+        //var clients = @Html(Json.stringify(Json.toJson(responses.map(x => x._1 -> x._2.history.map(y => y.clients)).toArray)));
+        
 //    responses.foreach(x => {
 //      println(x._1)
 //      println(x._2.hourly.map(y => y._1 + ":" + y._2).mkString("\t"))
 //    })
-    Ok(views.html.compare.index(responses.filter(x => x._2 != null)))
+
+    Ok(views.html.compare.index(responses, dailyCate, dailyQueries, dailyClients))
   }
 
 }
