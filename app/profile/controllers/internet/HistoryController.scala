8384811572aca0397f181d48337fac53ff90c2de
huyhoangt2 <01659039498>
2018-04-20 09:26:38 +0700

    fix conflick when mergre code

--- app/profile/controllers/internet/HistoryController.scala
+++ app/profile/controllers/internet/HistoryController.scala
@@ -23,7 +23,6 @@ import com.ftel.bigdata.utils.StringUtil
 import profile.services.internet.response.History
 import profile.services.internet.response.Hourly
 import controllers.InternetContract
-import profile.services.internet.Common
 
 //case class InternetContract(tpTime: String,date: String,ct: String)
 
@@ -32,15 +31,15 @@ import profile.services.internet.Common
   * application's home page.
   */
 @Singleton
-class HistoryController @Inject()(cc: ControllerComponents) extends AbstractController(cc) with Secured {
+class HistoryController @Inject()(cc: ControllerComponents) extends AbstractController(cc) with Secured{
 
   val day = CommonService.getCurrentDay()
 
   val client = Configure.client
-//  val form = Form(
-//    mapping(
-//      "ct" -> text)(SearchContract.apply)(SearchContract.unapply))
-      
+  //  val form = Form(
+  //    mapping(
+  //      "ct" -> text)(SearchContract.apply)(SearchContract.unapply))
+
   val form = Form(
     mapping(
       "tpTime" -> text,
@@ -65,54 +64,49 @@ class HistoryController @Inject()(cc: ControllerComponents) extends AbstractCont
 
   def index(date: String) =  Authenticated { implicit request =>
     //println("DATE: " + date)
-    
-//    try {
-      val formValidationResult = form.bindFromRequest
-      if (!formValidationResult.hasErrors) {
-        val contract = formValidationResult.get.ct.trim()
-        val _type = formValidationResult.get.tpTime.trim()
-        val time = formValidationResult.get.date.trim()
-        
-        println(_type + ": " + time)
-        //println(formValidationResult.get.ct)
-        //println(formValidationResult.get.date)
-        //println(formValidationResult.get.tpTime)
-        if (StringUtil.isNullOrEmpty(contract)) {
-          //println(contract)
-          //Ok(views.html.profile.internet.history.index(form, username, HistoryService.get("day", date), date, "day"))
-          //Ok(views.html.profile.internet.history.index(form, username, HistoryService.get("week", "2018-02-08"), date, "week"))
-//          Ok(views.html.profile.internet.history.index(form, username, HistoryService.getAll("month", "2018-02-01"), date, "month"))
-          
-          if (StringUtil.isNullOrEmpty(_type) && StringUtil.isNullOrEmpty(time)) {
-            Ok(views.html.profile.internet.history.index(form, username, HistoryService.getAll("M", "02/2018"), "02/2018", "M"))
-          } else {
-          Ok(views.html.profile.internet.history.index(form,
-            request.session.get("username").get.toString,
-              HistoryService.getAll(_type, time),
-              time,
-              _type))
-          }
+
+    //    try {
+    val formValidationResult = form.bindFromRequest
+    if (!formValidationResult.hasErrors) {
+      val contract = formValidationResult.get.ct.trim()
+      val _type = formValidationResult.get.tpTime.trim()
+      val time = formValidationResult.get.date.trim()
+
+      println(_type + ": " + time)
+      //println(formValidationResult.get.ct)
+      //println(formValidationResult.get.date)
+      //println(formValidationResult.get.tpTime)
+      if (StringUtil.isNullOrEmpty(contract)) {
+        //println(contract)
+        //Ok(views.html.profile.internet.history.index(form, username, HistoryService.get("day", date), date, "day"))
+        //Ok(views.html.profile.internet.history.index(form, username, HistoryService.get("week", "2018-02-08"), date, "week"))
+        //          Ok(views.html.profile.internet.history.index(form, username, HistoryService.getAll("month", "2018-02-01"), date, "month"))
+        Ok(views.html.profile.internet.history.index(form,
+          request.session.get("username").get.toString,
+          HistoryService.getAll(_type, time),
+          time,
+          _type))
+      } else {
+        if (StringUtil.isNullOrEmpty(_type) && StringUtil.isNullOrEmpty(time)) {
+          Ok(views.html.profile.internet.history.indexContract(form,
+            request.session.get("username").get.toString, HistoryService.getContract("M", "02/2018", contract.toLowerCase()),
+            contract,
+            time,
+            "M"))
         } else {
-          if (StringUtil.isNullOrEmpty(_type) && StringUtil.isNullOrEmpty(time)) {
-            Ok(views.html.profile.internet.history.indexContract(form,
-              request.session.get("username").get.toString, HistoryService.getContract("M", "02/2018", contract.toLowerCase()),
-              contract,
-              "02/2018",
-              "M"))
-          } else {
           Ok(views.html.profile.internet.history.indexContract(form,
             request.session.get("username").get.toString, HistoryService.getContract(_type, time, contract.toLowerCase()),
-              contract,
-              time,
-              _type))
-          }
-          //Ok(views.html.profile.internet.history.indexContract(form, username, HistoryService.getContract("week", "2018-02-08", contract.toLowerCase()), contract, "week"))
-          //Ok(views.html.profile.internet.history.indexContract(form, username, HistoryService.getContract("month", "2018-02-01", contract.toLowerCase()), contract, "month"))
+            contract,
+            time,
+            _type))
         }
-      } else {
-        println("============")
-        Ok(views.html.profile.internet.history.index(form, request.session.get("username").get.toString, HistoryService.getAll("M", "02/2018"), "02/2018", "M"))
+        //Ok(views.html.profile.internet.history.indexContract(form, username, HistoryService.getContract("week", "2018-02-08", contract.toLowerCase()), contract, "week"))
+        //Ok(views.html.profile.internet.history.indexContract(form, username, HistoryService.getContract("month", "2018-02-01", contract.toLowerCase()), contract, "month"))
       }
+    } else {
+      println("============")
+      Ok(views.html.profile.internet.history.index(form, request.session.get("username").get.toString, HistoryService.getAll("M", "02/2018"), "02/2018", "M"))
+    }
 
   }
 
@@ -122,7 +116,6 @@ class HistoryController @Inject()(cc: ControllerComponents) extends AbstractCont
 
   def realtimeJson() =  withAuth { username => implicit request =>
     val streaming = HistoryService.getRealtime(day)
-    streaming.hourly.contract.map(x=> x._1 -> x._2).foreach(println)
     val jsRealtime = Json.obj(
       "numberOfContract" -> streaming.numberOfContract,
       "numberOfSession" -> streaming.numberOfSession,
