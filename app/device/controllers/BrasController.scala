dc6fda01e5f1405b217cfce1a8f709efa6a454e1
huyhoangt2 <01659039498>
2018-04-23 14:36:08 +0700

    refactor project

---
+++ app/device/controllers/BrasController.scala
@@ -0,0 +1,55 @@
+package controllers
+
+import play.api.mvc._
+import controllers.Secured
+import javax.inject.Inject
+import javax.inject.Singleton
+import play.api.mvc.AbstractController
+import play.api.mvc.ControllerComponents
+import service.BrasService
+import scala.concurrent.Await
+import scala.concurrent.duration.Duration
+import play.api.libs.json.Json
+
+
+/**
+  * This controller creates an `Action` to handle HTTP requests to the
+  * application's home page.
+  */
+@Singleton
+class BrasController @Inject()(cc: ControllerComponents) extends AbstractController(cc) with Secured{
+
+  def index =  withAuth { username => implicit request =>
+    try {
+      val bras = Await.result(BrasService.listTop100Bras, Duration.Inf)
+      Ok(device.views.html.bras(username, bras))
+    }
+    catch{
+      case e: Exception => Ok(device.views.html.bras(username,null))
+    }
+  }
+
+  def confirmLabel(id: String,time: String) = Action { implicit request =>
+    try{
+      val res =  Await.result(BrasService.confirmLabel(id,time), Duration.Inf)
+      Ok(Json.toJson(res))
+    }
+    catch{
+      case e: Exception => Ok("error")
+    }
+  }
+
+  def rejectLabel(id: String,time: String) = Action { implicit request =>
+    try{
+      val res =  Await.result(BrasService.rejectLabel(id,time), Duration.Inf)
+      Ok(Json.toJson(res))
+    }
+    catch{
+      case e: Exception => Ok("error")
+    }
+  }
+
+}
+
+
+
---
+++ app/profiles/controllers/profile/PaytvClientController.scala
@@ -0,0 +1,37 @@
+package controllers
+
+import javax.inject.Inject
+import javax.inject.Singleton
+import play.api.mvc.AbstractController
+import play.api.mvc.ControllerComponents
+import services.domain.CommonService
+import services.user.DashboardService
+import play.api.mvc._
+/**
+  * This controller creates an `Action` to handle HTTP requests to the
+  * application's home page.
+  */
+@Singleton
+class PaytvClientController @Inject() (cc: ControllerComponents) extends AbstractController(cc) with Secured{
+
+  /* Authentication action*/
+  def Authenticated(f: AuthenticatedRequest => Result) = {
+    Action { request =>
+      val username = request.session.get("username").get.toString
+      username match {
+        case "btgd@ftel" =>
+          f(AuthenticatedRequest(username, request))
+        case none =>
+          Redirect(routes.LoginController.index).withNewSession.flashing(
+            "success" -> "You are now logged out."
+          )
+      }
+    }
+  }
+
+  def index() = Authenticated { implicit request =>
+    val response = DashboardService.get()
+    Ok(profiles.views.html.paytv.index(response,request.session.get("username").get.toString))
+  }
+  
+}
