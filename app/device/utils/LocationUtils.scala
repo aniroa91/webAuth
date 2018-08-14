928df07fb0cdb44c3a6fab8e49090cc85884612f
huyhoangt2 <01659039498>
2018-08-14 14:22:55 +0700

    change ip postgres 179

--- app/device/utils/LocationUtils.scala
+++ app/device/utils/LocationUtils.scala
@@ -8,7 +8,10 @@ object LocationUtils {
     .map(x => (x(0),x(3))-> x(4))
     .toMap
 
-  def getRegion(code: String): String = MAP.find(x=> x._1._2 == code.trim).get._2
+  def getRegion(code: String): String = {
+    //println(code)
+    MAP.find(x=> x._1._2 == code.trim).get._2
+  }
 
   def getNameProvincebyCode(code: String): String = MAP.find(x=> x._1._2 == code.trim).get._1._1
   def getCodeProvincebyName(name: String): String = MAP.find(x=> x._1._1 == name.trim).get._1._2
