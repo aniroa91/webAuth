5b627bd3e2d0f57674d83c603008aae9e24304d3
huyhoangt2 <01659039498>
2019-05-28 11:25:00 +0700

    update chart weekly problem to table

--- app/device/services/ExportService.scala
+++ app/device/services/ExportService.scala
@@ -11,7 +11,7 @@ case class JsonKibana(dateTime: String, brasId: String, errorName: String, sever
 
 case class JsonOpsview(dateTime: String, brasId: String, serviceName: String, serviceStatus: String, message: String, deviceType: String)
 
-case class JsonInf(dateTime: String, host: String, user_down: Long, inf_down: Double, sf_error: Long, lofi_error: Long, rouge_error: Long, lost_signal: Long, sf_module: Long)
+case class JsonInf(dateTime: String, errorName: String, host: String, module: Int, index: Int, text: String)
 
 case class JsonSuyhao(dateTime: String, province: String, host: String, module: String, not_pass_suyhao: String, pass_suyhao: String)
 
@@ -43,19 +43,11 @@ object ExportService extends AbstractService{
   def exportByInf(fromDate: String, toDate: String, device: String)= {
     val bras = "%"+device+"%"
     implicit val getUserResult = GetResult(r =>
-      JsonInf(r.nextString, r.nextString, r.nextLong, r.nextDouble,r.nextLong,r.nextLong,r.nextLong,r.nextLong,r.nextLong))
+      JsonInf(r.nextString, r.nextString, r.nextString, r.nextInt, r.nextInt, r.nextString))
     dbConfig.db.run(
-      sql"""select date_trunc('day', date_time) as date_time, host,
-                   sum(user_down) user_down,
-                   sum(inf_down) / 2 inf_down,
-                   sum(sf_error) sf_error,
-                   sum(lofi_error) lofi_error,
-                   sum(rouge_error) rouge_error,
-                   sum(lost_signal) lost_signal,
-                   sum(jumper_error) sf_module
-            from dwh_inf_host where host like $bras AND date_time >= $fromDate::TIMESTAMP AND date_time < $toDate::TIMESTAMP
-            group by date_trunc('day', date_time), host
-            order by date_trunc('day', date_time), host
+      sql"""select *
+            from dwh_inf_error where host like $bras AND date_time >= $fromDate::TIMESTAMP AND date_time < $toDate::TIMESTAMP
+            order by date_time desc
             """
         .as[JsonInf])
   }
